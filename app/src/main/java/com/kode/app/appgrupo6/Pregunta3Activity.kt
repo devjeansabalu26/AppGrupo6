6b2ccd78ee28947b11af7cd89533d87da77b8ea8
hideki <hidekimiguel10@gmail.com>
2026-09-22 21:45:50 -0500

    Agrega Pregunta3Activity

---
+++ app/src/main/java/com/kode/app/appgrupo6/Pregunta3Activity.kt
@@ -0,0 +1,20 @@
+package com.kode.app.appgrupo6
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Pregunta3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pregunta3)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
